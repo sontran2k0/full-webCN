169dc3faacaa8ef704840f6c428a58e94b5843ca
sontran2k0 <87573579+sontran2k0@users.noreply.github.com>
2021-09-29 21:37:12 +0700

    Add files via upload

---
+++ backend/data.js
@@ -0,0 +1,77 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    products:[
+        {
+        _id: '1',
+        name:' Trà sữa trân châu đường đen',
+        category: 'Tra',
+        image: '/images/t2.png',
+        tt: '/images/tt.jpg',
+        description:'Loại trà đặc trưng của vùng đồi núi A Lý ở Đài Loan, vị trà thanh nhẹ, hơi chát.',
+        price: 50000 ,
+        brand: 'Gongcha',
+        rating: 5
+
+    },
+    {   
+        _id: '2',
+        name:' Trà sữa Okinawa Oreo Cream',
+        category: 'Tra',
+        image: '/images/t1.png',
+        tt: '/images/tt.jpg',
+        price: 50000,
+        brand: 'Gongcha',
+        description:'Vị kẹo bơ cứng (Toffee) hòa quyện với trà sữa Earl Grey tạo nên mùi thơm đặc trưng của Hokkaido.',
+        rating: 5
+
+    },
+    {
+        _id: '3',
+        name:' Sữa tươi đường đen',
+        category: 'Tra',
+        image: '/images/t3.png',
+        tt: '/images/tt.jpg',
+        price: 50000,
+        brand: 'Gongcha',
+        description:'Là sự kết hợp truyền thống giữa trân châu đen dai cùng vị trà sữa trà đen. Thức uống quen thuộc với mọi lứa tuổi.',
+        rating: 5
+
+    },
+    {
+        _id: '4',
+        name:' Trà sữa Earl Grey',
+        category: 'Tra',
+        image: '/images/t4.png',
+        tt: '/images/tt.jpg',
+        price: 50000,
+        brand: 'Gongcha',
+        description:'Trà Bá Tước chiết xuất từ hương thảo mộc của quả Bergamot (loại quả lai giữa Cam và Chanh), có vị thơm nồng.',
+        rating: 5
+
+    },
+    {
+        _id: '5',
+        name:' Trà sữa  Alisan vị Xoài',
+        category: 'Tra',
+        image: '/images/t5.png',
+        tt: '/images/tt.jpg',
+        price: 50000,
+        brand: 'Gongcha',
+        description:'Trà Alisan đặc trưng hòa cùng vị Xoài ngọt ngào.',
+        rating: 5
+
+    },
+    {
+        _id: '9',
+        name:' Trà sữa dâu tây',
+        category: 'Tra',
+        image: '/images/t6.png',
+        tt: '/images/tt.jpg',
+        price: 50000,
+        brand: 'Gongcha',
+        description:'Sữa Yakult xay cùng với hương Dâu thơm ngọt, cho ra ly đá xay ngon miệng vừa có chua, có ngọt, vừa tốt cho sức khỏe, dùng kèm với Thạch Trái cây.',
+        rating: 5
+
+    },
+]
+}
